361d168adfd5388b3672749033f5c04e805d4bfd
jolo988 <126200054+jolo988@users.noreply.github.com>
2023-02-28 00:27:50 -0500

    Update checkmypass.py

--- checkmypass.py
+++ checkmypass.py
@@ -1,7 +1,9 @@
-import hashlib
+import hashlib #sha1 password hashing
 import requests
 import sys
 
+
+# check password against API
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
@@ -9,65 +11,56 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again.')
     return res
 
-# query_char to call the function with (make it dynamic)
-# Raise runtime error if not 200; if not return res (response)
-# request_api_data funct --> Supposed to get a response number
 
+# obtain number of password leaks
 def get_password_leak_count(hashes, hash_to_check):
+    
+    #tuple comprehension = line.split(':') = split everything in the line from the ':'
     hashes = (line.split(':') for line in hashes.text.splitlines())
+    
+    # iterate through h (tail end of hashed pw in API database)
     for h, count in hashes:
+        
+        # if h matches user input tail end hashed pw -> return count
         if h == hash_to_check:
             return count
+    
+    # return 0 if no password breaches
     return 0
 
-# hash = hash PWs in response to 1st 5 characters (the response: these are already tailed)
-# -> doesn't have first 5 char
-# hash to check = the TAIL: our pw that we have to check
-# Use hash to check to loop thru all hashes
-# tuple comprehension = line.split(':') = split everything in the line from the ':'
-# ->for each line -> Remove ':'
-# splitlines() = returns a list in the line as a string (so it's not broken into individual elements)
-# -> or else for loop runs it individually
-# h = each tailed hash pw within hashes (list of pw based on 1st 5 char)
-# count = # of times hacked
-# for h, count --> each item (hashed pw and # of times hacked are separated)
-# if h (found hashed pw) matches hash_to_check(our pw) -> return count, if not return 0
 
-
+# securely check password by converting to has object
 def pwned_api_check(password):
+
+    # split first 5 characters, and tail (rest of hash)
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
+
+    # Check first 5 characters of hashed pw against API; return response and hashed tail
     response = request_api_data(first5_char)
     return get_password_leak_count(response, tail)
 
-# haslib.sha1 wraps password.encode(utf-8) in a hash -> Object location
-# Hexdigest - returns string object of double length, containing only hexadecimal digits
-# -> converts hash object location into actual sha1 hash
-# pwned_api_check -> converts pw to hash
-# + calls get_pw_leak_count (returns response as txt (Hashed pw:count - how many times it got pwned))
-# + calls request_api_data (checks response (if >5 characters -> 400+ status code))
-# also breaking into hash bc it's more secure to check
 
+# main receives all the arguments (1 or more passwords) at command line
 def main(args):
+    
+    #iterate through API with args (passwords)
     for password in args:
         count = pwned_api_check(password)
         if count:
             print(f'{password} was found {count} times.. you should probably change your password')
         else:
             print(f'{password} was NOT hacked, carry on!')
+    
+    # return when action is completed
     return 'done'
 
-# print out all the results of every calculation
-# main receives all the arguments that we give it in command line
-# args = can give it multiple pws
-# loop thru passwords from args we give
 
+# run if __main__; exit file in case terminal doesn't exit, and brings user back to command line
+# [1:] -> accept any number of arguments when checking python3 checkmypass.py (add pw)
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
-# only run if this is the __main__ file
-# sys.exit = exits file in case terminal doesn't exit, and brings user back to command line
-# --> exiting out of the file -> Return done (for security reasons)
 
-# [1:] -> accept any number of arguments when checking python3 checkmypass.py (add pw)
+
 
 
